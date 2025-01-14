19acd2e95ee6059d30b503a036f54770264d8ed1
Andrii Sierov <151635663+AndriiSierov12344@users.noreply.github.com>
2025-01-14 10:56:56 +0200

    Update variables.tf

--- modules/webapps/static_site/variables.tf
+++ modules/webapps/static_site/variables.tf
@@ -48,10 +48,6 @@ variable "global_settings" {
   description = "Global settings object (see module README.md)"
 }
 
-variable "base_tags" {
-  description = "Base tags for the resource to be inherited from the resource group."
-  type        = map(any)
-}
 
 variable "diagnostic_profiles" {
   default = {}
