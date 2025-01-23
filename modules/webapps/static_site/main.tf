123e448803a97e8d9684272938d1f198fad10abd
Andrii Sierov <151635663+AndriiSierov12344@users.noreply.github.com>
2025-01-23 10:28:26 +0200

    Update main.tf

--- modules/webapps/static_site/main.tf
+++ modules/webapps/static_site/main.tf
@@ -23,5 +23,6 @@ locals {
     try(var.tags,
     null)
   )
-resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
+  location            = coalesce(var.location, var.resource_group.location)
+  resource_group_name = coalesce(var.resource_group_name, var.resource_group.name)
 }
