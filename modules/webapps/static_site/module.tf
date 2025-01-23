499d4af00f3cad6adbcdfbc098b8fac12136c9ac
Andrii Sierov <151635663+AndriiSierov12344@users.noreply.github.com>
2025-01-23 10:28:59 +0200

    Update module.tf

--- modules/webapps/static_site/module.tf
+++ modules/webapps/static_site/module.tf
@@ -12,7 +12,7 @@ resource "azurerm_static_site" "static_site" {
   name                = azurecaf_name.static_site.result
   location            = var.location
   resource_group_name = local.resource_group_name
-  tags                       = merge(local.tags, try(var.settings.tags, {}))
+  tags                = local.tags
 
   sku_size = var.sku_size
   sku_tier = var.sku_tier
